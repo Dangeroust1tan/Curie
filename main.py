d0c48d3c76ae128ae1ef0776f582a70a508ec155
Kushaal Dutta <kushaaldutta@gmail.com>
2022-07-06 23:02:58 +0530

    Jai Mata di

---
+++ main.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+import datetime
+import pocketsphinx
+
+r=sr.Recognizer()
+
+def googlelisten():
+    with sr.Microphone() as source:
+
+        audio=r.listen(source)
+
+        command= r.recognize_google(audio)
+
+        return command
+while True:
+    command=googlelisten()
+    print(command)
+    break
+
