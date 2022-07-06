aeba12b660d53d2caed943c9b4cf0cf050b8fad1
Kushaal Dutta <kushaaldutta@gmail.com>
2022-07-06 23:19:47 +0530

    Jai Mata di

--- main.py
+++ main.py
@@ -12,11 +12,19 @@ def googlelisten():
 
         command= r.recognize_google(audio)
         return command
+
+def sphinxlisten():
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+
+            command = r.recognize_sphinx(audio)
+            return command
 def getdate():
     return datetime.datetime.now().strftime('%I:%M %p')
 while True:
     command=googlelisten()
-    print(command)
+    command2=sphinxlisten()
+    print(command,command2,sep='  #  ')
     if 'time' in command:
         print(getdate())
     break
